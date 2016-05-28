f1ac247d518a0942eaa50c639999ebd9880f5b6f
Tamas Szekeres <szekerest@gmail.com>
2016-05-28 21:02:51 +0200

    Add missing files

---
+++ MapLibrary/NavigationHistory.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DMS.MapLibrary
+{
+    public class NavigationHistory
+    {
+        int current;
+        private List<NavigationHistoryItem> history;
+        public NavigationHistory()
+        {
+            history = new List<NavigationHistoryItem>();
+            current = -1;
+        }
+
+        public void Add(NavigationHistoryItem item)
+        {
+            if (++current < history.Count)
+                history.RemoveRange(current, history.Count - current);
+            history.Add(item);
+        }
+
+        public void First()
+        {
+            if (history.Count > 0)
+                current = 0;
+        }
+
+        public bool HasNext()
+        {
+            return (history.Count > 0) && (current != history.Count - 1);
+        }
+
+        public void Next()
+        {
+            if (HasNext())
+            {
+                ++current;
+            }
+        }
+
+        public bool HasPrevious()
+        {
+            return current > 0;
+        }
+
+        public void Previous()
+        {
+            if (HasPrevious())
+            {
+                --current;
+            }
+        }
+
+        public NavigationHistoryItem Current
+        {
+            get
+            {
+                return history[current];
+            }
+        }
+
+        public NavigationHistoryItem this[int number]
+        {
+            get
+            {
+                if (number >= 0 && number < history.Count)
+                {
+                    return history[number];
+                }
+                else
+                    throw new ArgumentOutOfRangeException();
+            }
+            set
+            {
+                if (number >= 0 && number < history.Count)
+                {
+                    history[number] = value;
+                }
+                else
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public void Clear()
+        {
+            history.Clear();
+            current = -1;
+        }
+    }
+}
---
+++ MapLibrary/NavigationHistoryItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OSGeo.MapServer;
+
+namespace DMS.MapLibrary
+{
+    public class NavigationHistoryItem
+    {
+        private double minx;
+        private double miny;
+        private double maxx;
+        private double maxy;
+        public NavigationHistoryItem(rectObj extent)
+        {
+            minx = extent.minx;
+            miny = extent.miny;
+            maxx = extent.maxx;
+            maxy = extent.maxy;
+        }
+
+        public void Apply(mapObj map)
+        {
+            map.setExtent(minx, miny, maxx, maxy);
+        }
+
+        public rectObj GetExtent()
+        {
+            return new rectObj(minx, miny, maxx, maxy, 0);
+        }
+    }
+}
